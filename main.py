e682a4066eda8a7ba36970768f2892f90462a6a1
LucSal6868 <125786165+LucSal6868@users.noreply.github.com>
2024-10-05 17:31:23 -0400

    more stuff

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import gemini  # Ensure this is your gemini.py file
 import time
+import  random
 
 # Initialize Streamlit session state
 if 'bots_response' not in st.session_state:
@@ -8,29 +9,43 @@ if 'bots_response' not in st.session_state:
 paused = False
 starter = 0;
 
+#Questions premade
+questions = ["Ketchup is a smoothie",
+             "Water is wet",
+             "A hot dog is a sandwich",
+             "A poptart is a ravioli",
+             "Cereal is a soup",
+             "A straw only has 1 hole, not 2"
+             ]
+
 # Streamlit UI
-st.title("⚔Beef Bots⚔️")
-user_input = st.text_area("Enter a statement:", height=150)
+st.title("⚔ Beef Bots ⚔")
+user_input = st.text_input("Enter a statement:", random.choice(questions))
+
+colors = ["red", "blue"]
 
 def start_beef():
     index = 0
     while not paused:
-
-        new_variable = gemini.get_next() + ":[blue]"
+        new_variable : str = f":{colors[index % 2]}[" + gemini.get_next() +"]"
         if st.write(new_variable, key="next_response_button" + str(index)):  # Button to get the next response
             st.session_state.bots_response.append(new_variable)  # Store the next bot's response
         index += 1
+
+
         time.sleep(5)
 
 
 
-if st.button("Chat"):
+if st.button("Chat", key="chatbutton"):
     start_beef()
+    del st.session_state["chatbutton"]
 
 
 # Pause button
 if st.button("⏸️", key="pause_button"):  # Pause button with a pause symbol
-    paused = True
+    #paused = True
+    pass
 
 # Continue conversation loop
 
