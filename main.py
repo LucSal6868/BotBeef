52e5499c5292636efe0e863b77b838fec4c04be7
LucSal6868 <125786165+LucSal6868@users.noreply.github.com>
2024-10-05 16:00:59 -0400

    Changed buttons

--- main.py
+++ main.py
@@ -6,30 +6,27 @@ import time
 if 'bots_response' not in st.session_state:
     st.session_state.bots_response = []
 paused = False
-
 starter = 0;
 
 # Streamlit UI
-st.title("BotBeef")
-user_input = st.text_area("Ask a question:", height=150)
+st.title("Banter Bots")
+user_input = st.text_area("Enter a statement:", height=150)
 
 def start_beef():
     index = 0
     while not paused:
 
         new_variable = gemini.get_next()
-        if st.button(new_variable, key="next_response_button" + str(index)):  # Button to get the next response
+        if st.write(new_variable, key="next_response_button" + str(index)):  # Button to get the next response
             st.session_state.bots_response.append(new_variable)  # Store the next bot's response
         index += 1
         time.sleep(5)
 
 
-if st.button("Send"):
-    # Set the user statement for the bots
-    # gemini.set_statement(user_input)
+
+if st.button("Chat"):
     start_beef()
-    # first_response = gemini.get_next()  # Get the response from the first bot
-    # st.session_state.bots_response.append(first_response)  # Store the first bot's response
+
 
 # Pause button
 if st.button("⏸️", key="pause_button"):  # Pause button with a pause symbol
